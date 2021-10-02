5932c6cad53eb6c7309194731e645093108bb83a
leonadro <ndm04004@gmail.com>
2021-10-02 15:22:03 +0900

    Create project

---
+++ .postcssrc.js
@@ -0,0 +1,18 @@
+// ESM
+// CommonJS
+// import autoprefixer from 'autoprefixer'
+const autoprefixer = require('autoprefixer')
+
+
+
+// export
+// prugins: [
+//     autoprefixer
+// ]
+
+
+module.exports = {
+    plugins: [
+        autoprefixer
+    ]
+}
